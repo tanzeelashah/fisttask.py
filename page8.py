1d5b12974d6a7449522d679fc164d1a58b59c78e
tanzeela shah <tanzeelaajmeershah@gmail.com>
2025-06-01 11:40:33 +0500

    Add files via upload

---
+++ page8.py
@@ -0,0 +1,47 @@
+#question 1
+print("learning python is fun!")
+#question 2
+integerli = 42
+print("integer literals",integerli)
+floatli = 3.14
+print("float literal",floatli)
+#question 3
+print('"used double quote inside single quotes"')
+#question 4
+boolenliteral1= True
+print(boolenliteral1)
+boolenliteral2= False
+print(boolenliteral2)
+#question 5
+a= ""
+print(a)
+#question 6
+a="hello"
+print(type(a))
+b=10
+print(type(b))
+c=3.5
+print(type(c))
+d=True
+print(type(d))
+e=None
+print(type(e))
+#question 7
+print("python's simplicity is powerful")
+#question 8
+fistname=input("enter your fist name:")
+lastname =input("enter your last name:")
+print("fullname is ",fistname+lastname)
+#question 9
+list=["11","ali","True"]
+print(list)
+#question 10
+print("""this is mutli line string.""")
+#question 11
+print(None == 0)
+print(None == False)
+#give both answer false beacause boolen cosider 0 is a false
+
+
+
+
